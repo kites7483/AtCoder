ad19d9208c8e466a5690906b9da730f2d3639506
Tomoya Kinoshita <poteti908@yahoo.co.jp>
2020-09-26 17:30:41 +0900

    ABC167,168,169追加

---
+++ Desktop/AtCoder/ABC169/169b.py
@@ -0,0 +1,14 @@
+n = int(input())
+line = list(map(int, input().split()))
+total = 1
+if 0 in line:
+  print(0)
+else:
+  for i in range(n):
+    total *= line[i]
+    if total > 10 ** 18:
+      break;
+  if total > 10 ** 18:
+    print(-1)
+  else:
+    print(total)
