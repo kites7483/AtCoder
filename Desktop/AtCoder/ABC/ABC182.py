35d0af1acdae7bffc6ced0cc1e631a5ee4195886
Tomoya Kinoshita <poteti908@yahoo.co.jp>
2020-11-10 11:19:57 +0900

    ABC182更新

--- Desktop/AtCoder/ABC/ABC182.py
+++ Desktop/AtCoder/ABC/ABC182.py
@@ -70,3 +70,64 @@ for i in range(n):
   point_list.append(point + positive_max)
   point += line[i]
 print(max(point_list))
+
+#E - Akari (pypy3)
+
+import copy
+import itertools
+
+h,w,n,m = map(int, input().split())
+matrix = [[0 for i in range(w)] for i in range(h)]
+light_list = []
+for i in range(n):
+  line = list(map(int, input().split()))
+  light_list.append(line)
+for i in range(m):
+  line = list(map(int, input().split()))
+  matrix[line[0]-1][line[1]-1] = -1
+  
+matrix_copy = copy.deepcopy(matrix)
+
+#横方向
+for point in light_list:
+  a,b = point[0],point[1]
+  if matrix[a-1][b-1] == 0:
+    matrix[a-1][b-1] = 1
+    #右方向
+    while b < w:
+      if matrix[a-1][b] != -1:
+        matrix[a-1][b] = 1
+        b += 1
+      else:
+        break
+    #左方向
+    while b > 1:
+      if matrix[a-1][b-2] != -1:
+        matrix[a-1][b-2] = 1
+        b -= 1
+      else:
+        break
+
+#縦方向
+for point in light_list:
+  a,b = point[0],point[1]
+  if matrix_copy[a-1][b-1] == 0:
+    matrix_copy[a-1][b-1] = 1
+    #下方向
+    while a < h:
+      if matrix_copy[a][b-1] != -1:
+        matrix_copy[a][b-1] = 1
+        a += 1
+      else:
+        break
+    #上方向
+    while a > 1:
+      if matrix_copy[a-2][b-1] != -1:
+        matrix_copy[a-2][b-1] = 1
+        a -= 1
+      else:
+        break
+
+matrix_add = list(map(lambda x,y: list(map(lambda a,b: a+b, x,y)), matrix, matrix_copy))
+matrix_flat = list(itertools.chain.from_iterable(matrix_add))
+print(sum(x>0 for x in matrix_flat))
