a95fbd8c450a2ae9598a943472ad8d4249fb8de2
Tomoya Kinoshita <poteti908@yahoo.co.jp>
2020-11-15 12:07:51 +0900

    お役立ち関数(Union-Find)

--- Desktop/AtCoder/useful_function.py
+++ Desktop/AtCoder/useful_function.py
@@ -20,3 +20,43 @@ for i in range(2,N+1):
   g1.append(( g1[-1] * i ) % mod) #(インデックス番号)!のmod
   inverse.append(( -inverse[mod % i] * (mod//i) ) % mod)
   g2.append((g2[-1] * inverse[-1]) % mod)
+
+#----------------------------
+#グループの合体及び同じグループに所属しているかの判定を行う（Union-Find）（https://juppy.hatenablog.com/entry/2018/11/08/蟻本_python_Union-Find木_競技プログラミング）
+#----------------------------
+
+#xの根を求める
+def find(x):
+    if par[x] < 0:
+        return x
+    else:
+        par[x] = find(par[x])
+        return par[x]
+
+#xとyの属する集合の併合
+def unite(x,y):
+    x = find(x)
+    y = find(y)
+    
+    if x == y:
+        return False
+    else:
+        #sizeの大きいほうがx
+        if par[x] > par[y]:
+            x,y = y,x
+        par[x] += par[y]
+        par[y] = x
+        return True
+
+#xとyが同じ集合に属するかの判定
+def same(x,y):
+    return find(x) == find(y)
+
+#xが属する集合の個数
+def size(x):
+    return -par[find(x)]
+
+n,m = map(int,input().split())
+#初期化
+#根なら-size,子なら親の頂点
+par = [-1]*n
