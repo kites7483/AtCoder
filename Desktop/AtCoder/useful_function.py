6f2b6308d5245937c8b284dfad68528b738dad8e
Tomoya Kinoshita <poteti908@yahoo.co.jp>
2020-11-15 12:42:29 +0900

    お役立ち関数(高速素因数分解)

--- Desktop/AtCoder/useful_function.py
+++ Desktop/AtCoder/useful_function.py
@@ -60,3 +60,29 @@ n,m = map(int,input().split())
 #初期化
 #根なら-size,子なら親の頂点
 par = [-1]*n
+
+#----------------------------
+#高速素因数分解 (https://qiita.com/snow67675476/items/e87ddb9285e27ea555f8）
+#----------------------------
+
+#n >= 2
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    if arr==[]:
+        arr.append([n, 1])
+
+    return arr
+
+factorization(24) #[[2,3],[3,1]]
